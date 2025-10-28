f63dc6fbe21e160335cd3a7c350a4d998e95b2fb
Naman Garg <jokerace146@gmail.com>
2025-10-28 23:26:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function rollDice(){
+    
+    const numofdice = document.getElementById("num").value;
+    const diceresult = document.getElementById("dice_result");
+    const diceimages = document.getElementById("dice_images");
+    const values = [];
+    const images = [];
+    
+    for(let i = 0; i < numofdice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dicepng/${value}.png">`);    
+    }
+
+    diceresult.textContent = `DICE RESULT: ${values.join('/')}` ;
+    diceimages.innerHTML = images.join('');
+}
