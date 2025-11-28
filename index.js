847a6c66c6d2e82ab2faace56b7f984c363263b2
Naman Garg <jokerace146@gmail.com>
2025-11-28 19:00:26 +0530

    Enhance rollDice function to include total
    Added total calculation for rolled dice.

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ function rollDice(){
     const numofdice = document.getElementById("num").value;
     const diceresult = document.getElementById("dice_result");
     const diceimages = document.getElementById("dice_images");
+    const dicetotal = document.getElementById("dice_total");
     const values = [];
     const images = [];
+    let total = 0;
     
     for(let i = 0; i < numofdice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
-        images.push(`<img src="${value}.png">`);    
+        images.push(`<img src="${value}.png">`);  
+        total += i;
     }
 
     diceresult.textContent = `DICE RESULT: ${values.join('/')}` ;
     diceimages.innerHTML = images.join('');
-
+    dicetotal.textContent = `DICE TOTAL: ${total}`;
 }
+
